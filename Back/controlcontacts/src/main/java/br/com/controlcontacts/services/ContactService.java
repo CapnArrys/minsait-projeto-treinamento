1527230af62cdf515e47287efd40ba07405a0ef1
CapnArrys <arryshanry@gmail.com>
2023-10-28 15:45:45 -0300

    Projeto API

--- Back/controlcontacts/src/main/java/br/com/controlcontacts/services/ContactService.java
+++ Back/controlcontacts/src/main/java/br/com/controlcontacts/services/ContactService.java
@@ -1,5 +1,68 @@
 package br.com.controlcontacts.services;
 
-public class ContactService {
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.controlcontacts.exception.ResourceNotFoundException;
+import br.com.controlcontacts.entities.Contact;
+import br.com.controlcontacts.repository.ContactRepository;
+import br.com.controlcontacts.service.interfaces.ContactServiceInterface;
+
+	@Service
+	public class ContactService implements ContactServiceInterface {
+
+		private ContactRepository contactRepository;
+		
+		@Autowired
+		public ContactService(ContactRepository contactRepository) {
+			this.contactRepository = contactRepository;
+		}
+		
+		@Override
+		public Contact save(Contact contact) {
+			return contactRepository.save(contact);
+		}
+
+		@Override
+		public Optional<Contact> getById(Long id) {
+			return contactRepository.findById(id);
+		}
+
+		@Override
+		public List<Contact> getAll() {
+			return contactRepository.findAll();
+		}
+
+		@Override
+		public Contact update(Contact contact) {
+			//encontrei o produto
+			Optional<Contact> upContact = contactRepository.findById(contact.getId());
+			
+			//se ele existir, atualizar:
+			if(upContact.isPresent()) {
+				Contact newContact = upContact.get();
+				newContact.setId(contact.getId());
+				newContact.setContact(contact.getContact());
+				newContact.setContactType(contact.getContactType());
+				return contactRepository.save(newContact);
+			}
+			
+			
+			return contact;
+		}
+
+		@Override
+		public void delete(Long id) {
+			contactRepository.deleteById(id);
+			
+		}
+
+
+
+	
 
 }
--- Back/controlcontacts/src/main/java/br/com/controlcontacts/services/PersonService.java
+++ Back/controlcontacts/src/main/java/br/com/controlcontacts/services/PersonService.java
@@ -1,5 +1,57 @@
 package br.com.controlcontacts.services;
 
-public class PersonService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.controlcontacts.exception.ResourceNotFoundException;
+import br.com.controlcontacts.entities.Person;
+import br.com.controlcontacts.repository.PersonRepository;
+import br.com.controlcontacts.service.interfaces.PersonServiceInterface;
+
+
+@Service
+public class PersonService implements PersonServiceInterface {
+	
+	private PersonRepository personRepository;
+	
+	@Autowired
+	public PersonService(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
+	
+	@Override
+	public Person save(Person person) {
+		return personRepository.save(person);
+	}
+
+	@Override
+	public Optional<Person> getById(Long id) {
+		return personRepository.findById(id);
+	}
+
+	@Override
+	public List<Person> getAll() {
+		return personRepository.findAll();
+	}
+
+	@Override
+	public Person update(Person person) {
+		Optional<Person> upPerson = personRepository.findById(person.getId());
+		
+		if(upPerson.isPresent()) {
+			Person newPerson = upPerson.get();
+			newPerson.setContacts(person.getContacts());
+			return personRepository.save(newPerson);
+		}
+		return person;
+	}
+
+	@Override
+	public void delete(Long id) {
+		personRepository.deleteById(id);
+	}
+	
 }
